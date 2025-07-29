31d19456c21291d1707fb7be61e83cc8b463387b
Marcellino Timothy Martua Hutasoit <119827618+Acel05@users.noreply.github.com>
2025-07-29 10:20:55 +0700

    Update adhoc_train_model.php

--- studentperformancepredictor/classes/task/adhoc_train_model.php
+++ studentperformancepredictor/classes/task/adhoc_train_model.php
@@ -1,26 +1,5 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Ad-hoc task for training models on demand.
- *
- * @package    block_studentperformancepredictor
- * @copyright  2023 Your Name <[Email]>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+// classes/task/adhoc_train_model.php
 
 namespace block_studentperformancepredictor\task;
 
