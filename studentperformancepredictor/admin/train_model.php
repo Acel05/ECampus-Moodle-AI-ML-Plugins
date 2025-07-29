5cba16b0c7dfcfdcddd182c679db80fd8fa018a5
Marcellino Timothy Martua Hutasoit <119827618+Acel05@users.noreply.github.com>
2025-07-29 10:22:39 +0700

    Update train_model.php

--- studentperformancepredictor/admin/train_model.php
+++ studentperformancepredictor/admin/train_model.php
@@ -1,26 +1,5 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Train model handler for Student Performance Predictor.
- *
- * @package    block_studentperformancepredictor
- * @copyright  2023 Your Name <[Email]>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+// admin/train_model.php
 
 // Basic Moodle config
 require(__DIR__ . '/../../../config.php');
@@ -106,6 +85,10 @@ if (training_manager::has_pending_training($courseid)) {
     }
 }
 
+// Ensure debug mode is on temporarily to capture more information
+$original_debug = $CFG->debug;
+$CFG->debug = DEBUG_DEVELOPER;
+
 // Schedule training (backend-driven orchestration)
 try {
     // This will queue a training task that calls the Python backend /train endpoint
@@ -116,7 +99,11 @@ try {
     }
 } catch (\Exception $e) {
     \core\notification::error($e->getMessage());
+    error_log("Training error: " . $e->getMessage() . "\n" . $e->getTraceAsString());
 }
+
+// Restore original debug setting
+$CFG->debug = $original_debug;
 
 // Redirect back to appropriate page
 if ($courseid == 0) {
